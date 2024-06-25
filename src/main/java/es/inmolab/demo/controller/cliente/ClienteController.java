13b39b583073b4a034d5c083023c2bb8b58b4b12
Richar Silva <richard.silva@nextcode.cl>
2024-06-25 23:23:57 +0200

    Agregar el messages.properties del proyecto

--- src/main/java/es/inmolab/demo/controller/cliente/ClienteController.java
+++ src/main/java/es/inmolab/demo/controller/cliente/ClienteController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import es.inmolab.demo.entity.Cliente;
@@ -23,7 +23,7 @@ public class ClienteController {
     }
 	
 	
-    @GetMapping
+    @PostMapping
     public String clientePorTipo(Model model) {
         List<Cliente> clientes = clienteService.obtieneClientesPorTipo(model);
         model.addAttribute("clientes", clientes);
