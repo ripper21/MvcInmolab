5e546490ddb4de4cd035f46d93d2f3232161f1f8
Richar Silva <richard.silva@nextcode.cl>
2024-07-04 20:34:48 +0200

    cambiar idioma del data table

--- bin/src/main/resources/static/js/scripts.js
+++ bin/src/main/resources/static/js/scripts.js
@@ -41,11 +41,16 @@ myModal.addEventListener('shown.bs.modal', function () {
 })
 
 window.addEventListener('DOMContentLoaded', event => {
-    // Simple-DataTables
-    // https://github.com/fiduswriter/Simple-DataTables/wiki
-
     const datatablesSimple = document.getElementById('datatablesSimple');
     if (datatablesSimple) {
-        new simpleDatatables.DataTable(datatablesSimple);
+        new simpleDatatables.DataTable(datatablesSimple, {
+            labels: {
+                placeholder: "Buscar...", // Placeholder del campo de búsqueda
+                perPage: "{select} registros por página", // Opción para seleccionar cuántos registros mostrar por página
+                noRows: "No se encontraron registros", // Mensaje cuando no se encuentran registros
+                info: "Mostrando registros del {start} al {end} de un total de {rows} registros", // Información de los registros mostrados
+            },
+            // Aquí puedes seguir añadiendo otras opciones y etiquetas necesarias
+        });
     }
 });
