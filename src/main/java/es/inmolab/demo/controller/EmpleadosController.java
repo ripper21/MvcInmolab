a3f5c1c8d298d509f678bd2355589b65cfa62ab0
Richar Silva <richard.silva@nextcode.cl>
2024-07-04 08:58:03 +0200

    Funcionalidades desde casa

--- src/main/java/es/inmolab/demo/controller/EmpleadosController.java
+++ src/main/java/es/inmolab/demo/controller/EmpleadosController.java
@@ -1,10 +1,14 @@
 package es.inmolab.demo.controller;
-
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,23 +17,55 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import es.inmolab.demo.dto.EmpleadoDto;
 import es.inmolab.demo.service.empleado.EmpleadoService;
-
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
 @Controller
 @RequestMapping("/empleados")
 public class EmpleadosController {
-	   Logger log = LoggerFactory.getLogger(EmpleadosController.class);
+
 
     private final EmpleadoService empleadoService;
-    
+    private final MessageSource messageSource;
+
     @Autowired
-    public EmpleadosController  (EmpleadoService empleadoService) {
+    public EmpleadosController(EmpleadoService empleadoService, MessageSource messageSource) {
         this.empleadoService = empleadoService;
+        this.messageSource = messageSource;
     }
 
     @GetMapping("/listar")
-    public String listarEmpleados(Model model) {
+    public String listarEmpleados(Model model, HttpServletRequest request) {
         List<EmpleadoDto> empleados = empleadoService.getAllEmpleados();
         model.addAttribute("empleados", empleados);
+
+        List<Integer> edades = empleados.stream()
+                .map(e -> Period.between(e.getFechNac().toInstant().atZone(ZoneId.systemDefault()).toLocalDate(), LocalDate.now()).getYears())
+                .collect(Collectors.toList());
+        model.addAttribute("edades", edades);
+        
+        List<String> nombres = empleados.stream()
+                .map(EmpleadoDto::getNombre)
+                .collect(Collectors.toList());
+        model.addAttribute("nombres", nombres);
+
+        Locale locale = request.getLocale();
+        List<String> months = Arrays.asList(
+                messageSource.getMessage("month.jan", null, locale),
+                messageSource.getMessage("month.feb", null, locale),
+                messageSource.getMessage("month.mar", null, locale),
+                messageSource.getMessage("month.apr", null, locale),
+                messageSource.getMessage("month.may", null, locale),
+                messageSource.getMessage("month.jun", null, locale),
+                messageSource.getMessage("month.jul", null, locale),
+                messageSource.getMessage("month.aug", null, locale),
+                messageSource.getMessage("month.sep", null, locale),
+                messageSource.getMessage("month.oct", null, locale),
+                messageSource.getMessage("month.nov", null, locale),
+                messageSource.getMessage("month.dec", null, locale)
+        );
+        model.addAttribute("months", months);
+
         return "empleadosList";  // Retorna a empleadosList.html
     }
     
